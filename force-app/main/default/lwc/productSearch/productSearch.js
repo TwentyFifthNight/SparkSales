3672cc884130c73454eb4ce76d350c93ad7e509b
Damian <damianweclawski@gmail.com>
2025-07-31 16:20:24 +0200

    Add Product Summary LWC and Update Product Search

--- force-app/main/default/lwc/productSearch/productSearch.js
+++ force-app/main/default/lwc/productSearch/productSearch.js
@@ -1,27 +1,64 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track, api} from 'lwc';
 import { getPicklistValues, getObjectInfo } from 'lightning/uiObjectInfoApi';
 import getRecordList from '@salesforce/apex/ProductSearchController.getRecordList';
 import getRecordCount from '@salesforce/apex/ProductSearchController.getRecordCount';
+import getSelectedProducts from '@salesforce/apex/ProductSearchController.getOpportunityProducts';
+import generateOrderItemsFromProducts from '@salesforce/apex/ProductSearchController.generateOrderItemsFromProducts';
+import createNewOrder from '@salesforce/apex/ProductSearchController.createNewOrder';
 import PRODUCT_OBJECT from '@salesforce/schema/Product2';
 import FAMILY_FIELD from '@salesforce/schema/Product2.Family';
 import { loadStyle } from 'lightning/platformResourceLoader';
 import NoHeader from '@salesforce/resourceUrl/noHeader';
+import { CloseActionScreenEvent } from 'lightning/actions';
+import searchPage from './productSearch.html';
+import summaryPage from './productListSummary.html';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { NavigationMixin } from "lightning/navigation";
 
-export default class ProductSearch extends LightningElement {
+const Page = {
+    ProductSearch: 'productSearch',
+    ProductSummary: 'productSummary'
+}
+
+export default class ProductSearch extends NavigationMixin(LightningElement) {
+    currentLWCPage = Page.ProductSearch;
     columns = [
         { label: 'Name', fieldName: 'Name', type: 'text'},
-        //{ label: 'Name', fieldName: 'URL', type: 'url', typeAttributes: { label: {fieldName: 'Name'}}},
         { label: 'Product Code', fieldName: 'ProductCode', type: 'text'},
         { label: 'Family', fieldName: 'Family', type: 'text'},
-        //{ type: 'action', typeAttributes: { rowActions: ACTIONS } }
+    ];
+    orderColumns = [
+        { label: 'Product Name', fieldName: 'name', type: 'text'},
+        { label: 'Quantity', fieldName: 'quantity', type: 'number', editable: true },
+        { label: 'Unit Price', fieldName: 'unitPrice', type: 'currency', editable: true},
     ];
     pageSizeOptions = [
         { label: '5', value: '5' },
         { label: '10', value: '10' },
         { label: '15', value: '15' },
-        { label: '20', value: '20' },
-        { label: '30', value: '30' }
+        { label: '20', value: '20' }
     ];
+
+    @track selectedProductIds  = [];
+    selectedProducts = [];
+    
+    _recordId = undefined;
+    @api set recordId(value) {
+        if(this._recordId === undefined){
+            getSelectedProducts({ opportunityId: value})
+                .then(result => {
+                    this.selectedProducts = result;
+                    this.selectedProductIds = result.map(record => record.Id);
+                })
+                .catch(error => {
+                    this.error = error;
+                });
+        }
+        this._recordId = value;
+    }
+    get recordId() {
+        return this._recordId;
+    }
 
     nameAndCode = '';
     family = '';
@@ -29,14 +66,25 @@ export default class ProductSearch extends LightningElement {
     familyOptions = [];
     error = undefined;
     isLoading = false;
-    nextBlock = false;
 
     recordsPerPage = 10;
     currentPage = 1;
-    localPages = 0;
     totalPages = 0;
-    LocalRecordsCount = 0;
-    
+    allRecordsCount = 0;
+    showSelected = false;
+    afterFirstSearch = false;
+    orderItems = undefined;
+    totalPrice = 0;
+
+    render() {
+        switch(this.currentLWCPage) {
+            case Page.ProductSearch:
+                return searchPage;
+            case Page.ProductSummary:
+                return summaryPage;
+        }
+    }
+
     connectedCallback() {
         loadStyle(this, NoHeader)
             .then(result => {});
@@ -63,27 +111,82 @@ export default class ProductSearch extends LightningElement {
         ]
     }
 
+    handleRowSelection(event){
+        switch (event.detail.config.action){
+            case 'selectAllRows':
+                for (let i = 0; i < event.detail.selectedRows.length; i++) {
+                    if (!this.selectedProductIds.includes(event.detail.selectedRows[i].Id)){
+                        this.selectedProductIds.push(event.detail.selectedRows[i].Id);
+                        this.selectedProducts.push(event.detail.selectedRows[i]);
+                    }
+                }
+                break;
+            case 'deselectAllRows':
+                let currentlyShownProducts = this.showSelected ? this.selectedProducts : this.products;
+                let currentlyShownProductIds = currentlyShownProducts.map(product => product.Id);
+                
+                this.selectedProducts = this.selectedProducts
+                    .filter(product => !currentlyShownProductIds.includes(product.Id));
+                this.selectedProductIds = this.selectedProducts.map(product => product.Id);
+                break;
+            case 'rowSelect':
+                let selectedId = event.detail.config.value;
+                if (!this.selectedProductIds.includes(selectedId)){
+                    this.selectedProductIds.push(selectedId);
+                    let selectedProduct = this.products.find(product => product.Id === selectedId);
+                    this.selectedProducts.push(selectedProduct);
+                }
+                break;
+            case 'rowDeselect':
+                const index = this.selectedProductIds.indexOf(event.detail.config.value);
+                if (index > -1) {
+                    this.selectedProductIds.splice(index, 1);
+                    this.selectedProducts.splice(index, 1);
+                }
+                break;
+            default:
+                break;
+        }
+        this.selectedProducts = [...this.selectedProducts];
+    }
+
     handleInputChange(event){
         this[event.target.name] = event.target.value;
     }
 
-    async handleSearchButton(event) {
-        this.currentPage = 1;
-        await this.getAccountPageCount();
-        await this.handleRecordRequest(1);
+    draftValues;
+    handleSummaryInputChange(event){
+        const changedFields = event.detail.draftValues;
+        const itemId = changedFields[0].product2Id;
+        const field = Object.keys(changedFields[0])[0];
+        const value = changedFields[0][field];
+        this.orderItems = this.orderItems.map(item => {
+            if (item.product2Id === itemId) {
+                return { ...item, [field]: Math.round(parseFloat(value) * 100) / 100 }; // Cut off to 2 decimal places
+            }
+            return item;
+        });
+        this.draftValues = []; // Remove highlighted rows from datatable
+        this.totalPrice = this.orderItems.reduce((total, item) => total + (item.quantity * item.unitPrice), 0);
     }
 
-    async getAccountPageCount(){
-        await this.getLocalRecordCount();
-        this.totalPages = Math.ceil(this.LocalRecordsCount/this.recordsPerPage);
+    async handleSearchButton(event) {
+        this.currentPage = 1;
+        await this.getPageCount();
+        await this.handleRecordRequest(1);
+        this.afterFirstSearch = true;
+    }
+
+    async getPageCount(){
+        await this.getRecordCount();
+        this.totalPages = Math.ceil(this.allRecordsCount/this.recordsPerPage);
     }
     
-    async getLocalRecordCount(){
+    async getRecordCount(){
         this.isLoading = true;
-        await getRecordCount({ parameters: { nameAndCode: this.nameAndCode, family: this.family } })
+        await getRecordCount({ parameters: { nameAndCode: this.nameAndCode, family: this.family, opportunityId: this.recordId }})
             .then(result => {
-                this.LocalRecordsCount = result;
-                this.localPages = Math.ceil(this.LocalRecordsCount/this.recordsPerPage);
+                this.allRecordsCount = result;
             })
             .catch(error => {
                 this.error = error;
@@ -92,13 +195,14 @@ export default class ProductSearch extends LightningElement {
     }
 
     async handleRecordRequest(pageNumber) {
-        await this.getLocalRecords(pageNumber);
+        await this.getRecords(pageNumber);
+        this.reloadSelectedRecords();
     }
 
-    async getLocalRecords(pageNumber){
+    async getRecords(pageNumber){
         this.isLoading = true;
         let offsetValue = this.recordsPerPage * (pageNumber - 1);
-        await getRecordList({ parameters: { nameAndCode: this.nameAndCode, family: this.family, offset: offsetValue, recordsPerPage: this.recordsPerPage } })
+        await getRecordList({ parameters: { nameAndCode: this.nameAndCode, family: this.family, offset: offsetValue, recordsPerPage: this.recordsPerPage, opportunityId: this.recordId} })
             .then(result => {
                 this.products = result;
             })
@@ -109,10 +213,14 @@ export default class ProductSearch extends LightningElement {
         this.isLoading = false;
     }
 
+    reloadSelectedRecords() {
+        this.selectedProductIds = [...this.selectedProductIds];
+    }
+
     handlePageSizeChange(event) {
         this.recordsPerPage = parseInt(event.detail.value);
         this.currentPage = 1;
-        this.totalPages = Math.ceil(this.LocalRecordsCount/this.recordsPerPage);
+        this.totalPages = Math.ceil(this.allRecordsCount/this.recordsPerPage);
         this.handleRecordRequest(this.currentPage);
     }
 
@@ -160,6 +268,10 @@ export default class ProductSearch extends LightningElement {
         return this.recordsPerPage.toString();
     }
 
+    get noResults() {
+        return !this.isLoading && this.products?.length === 0 && !this.error;
+    }
+
     resetFilters() {
         this.nameAndCode = '';
         this.family = '';
@@ -167,10 +279,96 @@ export default class ProductSearch extends LightningElement {
         this.error = undefined;
         this.currentPage = 1;
         this.totalPages = 0;
-        this.LocalRecordsCount = 0;
+        this.allRecordsCount = 0;
     }
 
-    get noResults() {
-        return !this.isLoading && this.products?.length === 0 && !this.error;
+    handleDismiss(){
+        this.dispatchEvent(new CloseActionScreenEvent());
+    }
+
+    async handleNextButton() {
+        if(this.selectedProducts.length < 1) {
+            const event = new ShowToastEvent({
+                title: 'Missing products',
+                message: 'Please select at least one product',
+                variant: 'error'
+            });
+            this.dispatchEvent(event);
+            return;
+        }
+
+        await this.getOrderItemList();
+        this.currentLWCPage = Page.ProductSummary;
+    }
+
+    async handleCreateButton(){
+        if(this.validateOrderItems() === false) {
+            return;
+        }
+        this.isLoading = true;
+        await createNewOrder({orderItems: this.orderItems, opportunityId: this.recordId})
+            .then(result => {
+                this[NavigationMixin.Navigate]({
+                    type: 'standard__recordPage',
+                    attributes: {
+                        recordId: result,
+                        actionName: 'view'
+                    }
+                });
+            })
+            .catch(error => {
+                this.error = error;
+            })
+            .finally(() => {
+                this.isLoading = false;
+            });
+    }
+
+    validateOrderItems() {
+        for (let i = 0; i < this.orderItems.length; i++) {
+            if (!(this.orderItems[i].quantity > 0) || !(this.orderItems[i].unitPrice > 0)) {
+                const event = new ShowToastEvent({
+                    title: 'Invalid data',
+                    message: `Quantity and Unit Price must be greater than 0 (${this.orderItems[i].name})`,
+                    variant: 'error'
+                });
+                this.dispatchEvent(event);
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    async getOrderItemList(){
+        this.isLoading = true;
+        await generateOrderItemsFromProducts({products: this.selectedProducts, opportunityId: this.recordId})
+            .then(result => {
+                this.orderItems = result;
+                this.totalPrice = this.orderItems.reduce((total, item) => total + (item.quantity * item.unitPrice), 0);
+            })
+            .catch(error => {
+                this.error = error;
+                this.orderItems = undefined;
+            })
+            .finally(() => {
+                this.isLoading = false;
+            });
+    }
+
+    get toggleListViewButtonLabel(){
+        return this.showSelected ? 'View All' : 'Show Selected';
+    }
+
+    toggleListViewType(){
+        this.showSelected = !this.showSelected;
+    }
+
+    get showAll(){
+        return !this.showSelected;
+    }
+
+    get showNextButton(){
+        return this.selectedProductIds.length > 0 && this.afterFirstSearch;
     }
 }
