a1c572953651c11307dcc0560787575c73963150
Damian <damianweclawski@gmail.com>
2025-08-07 11:08:06 +0200

    update productSearch test

--- force-app/main/default/lwc/productSearch/__tests__/productSearch.test.js
+++ force-app/main/default/lwc/productSearch/__tests__/productSearch.test.js
@@ -3,6 +3,19 @@ import ProductSearch from 'c/productSearch';
 import getRecordList from '@salesforce/apex/ProductSearchController.getRecordList';
 import getRecordCount from '@salesforce/apex/ProductSearchController.getRecordCount';
 
+const mockData = [
+    {
+        Name: 'Test Product',
+        ProductCode: 'GWE12',
+        Family: 'Networking'
+    },
+    {
+        Name: 'Test Product',
+        ProductCode: 'ASD12',
+        Family: 'Networking'
+    }
+]
+
 jest.mock(
     '@salesforce/apex/ProductSearchController.getRecordList',
     () => {
@@ -58,6 +71,7 @@ describe('c-product-search', () => {
             body: { message: 'An error occurred' },
             statusText: 'Error'
         });
+        getRecordList.mockResolvedValue(mockData);
         
         const element = createElement('c-product-search', {
             is: ProductSearch
@@ -98,18 +112,7 @@ describe('c-product-search', () => {
 
     it('should display records when data is available and search button is clicked', async () => {
         getRecordCount.mockResolvedValue(2);
-        getRecordList.mockResolvedValue([
-            {
-                Name: 'Test Product',
-                ProductCode: 'GWE12',
-                Family: 'Networking'
-            },
-            {
-                Name: 'Test Product',
-                ProductCode: 'ASD12',
-                Family: 'Networking'
-            }
-        ]);
+        getRecordList.mockResolvedValue(mockData);
         
         const element = createElement('c-product-search', {
             is: ProductSearch
@@ -133,14 +136,12 @@ describe('c-product-search', () => {
         const element = createElement('c-product-search', {
             is: ProductSearch
         });
-
         
         document.body.appendChild(element);
         const button = element.shadowRoot.querySelector('[data-id="search-button"]');
         button.click();
 
         await flushPromises();
-
         
         const datatable = element.shadowRoot.querySelector('lightning-datatable');
         expect(datatable).toBeNull();
@@ -150,12 +151,10 @@ describe('c-product-search', () => {
         expect(noRecords.textContent).toBe('No record found');
     });
 
-    it('should not display datatable error or no record message at the start', async () => {    
-        
+    it('should not display datatable, error or no record message after loading page', async () => {    
         const element = createElement('c-product-search', {
             is: ProductSearch
         });
-        
         
         document.body.appendChild(element);
         
@@ -167,5 +166,46 @@ describe('c-product-search', () => {
         expect(datatable).toBeNull();
         const noRecords = element.shadowRoot.querySelector('[data-id="no-records"]');
         expect(noRecords).toBeNull();
+    });
+
+    it('should not display datatable and clear inputs when reset filters button is pres', async () => {    
+        getRecordCount.mockResolvedValue(2);
+        getRecordList.mockResolvedValue(mockData);
+
+        const element = createElement('c-product-search', {
+            is: ProductSearch
+        });
+        
+        document.body.appendChild(element);
+
+        let nameCodeInput = element.shadowRoot.querySelector('[data-id="name-code-input"]');
+        nameCodeInput.value = 'Test Product';
+        nameCodeInput.dispatchEvent(new CustomEvent('change'));
+        
+        let categoryCombobox = element.shadowRoot.querySelector('[data-id="category-combobox"]');
+        categoryCombobox.value = 'Networking';
+        categoryCombobox.dispatchEvent(new CustomEvent('change'));
+
+        const button = element.shadowRoot.querySelector('[data-id="search-button"]');
+        button.click();
+        await flushPromises();
+
+        const datatable = element.shadowRoot.querySelector('lightning-datatable');
+        expect(datatable).not.toBeNull();
+        nameCodeInput = element.shadowRoot.querySelector('[data-id="name-code-input"]');
+        expect(nameCodeInput.value).toBe('Test Product');
+        categoryCombobox = element.shadowRoot.querySelector('[data-id="category-combobox"]');
+        expect(categoryCombobox.value).toBe('Networking');
+
+        const resetButton = element.shadowRoot.querySelector('[data-id="reset-button"]');
+        resetButton.click();
+        await flushPromises();
+
+        const datatableAfter = element.shadowRoot.querySelector('lightning-datatable');
+        expect(datatableAfter).toBeNull();
+        const nameCodeInputAfter = element.shadowRoot.querySelector('[data-id="name-code-input"]');
+        expect(nameCodeInputAfter.value).toBe('');
+        const categoryComboboxAfter = element.shadowRoot.querySelector('[data-id="category-combobox"]');
+        expect(categoryComboboxAfter.value).toBe('');
     });
 });
